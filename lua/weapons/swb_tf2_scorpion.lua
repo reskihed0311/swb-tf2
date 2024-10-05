8d7014b7959fd4289446ffa2053052e87fea0f16
NotTrashraD <bellie1579@gmail.com>
2024-10-05 18:23:16 +0200

    add workfiles + scorpion

---
+++ lua/weapons/swb_tf2_scorpion.lua
@@ -0,0 +1,93 @@
+AddCSLuaFile()
+
+SWEP.Base = "simple_base"
+
+SWEP.PrintName = "SCORPION"
+SWEP.Category = "reshed's Simple Weapons: TF2"
+SWEP.SubCategory = "Sub-Machine Guns"
+
+SWEP.CSMuzzleFlashes = true
+
+SWEP.Slot = 2
+
+SWEP.Spawnable = true
+
+SWEP.UseHands = true
+
+SWEP.ViewModelTargetFOV = 62
+
+SWEP.ViewModel = Model("models/weapons/tf2_reshed/r_scorpion.mdl")
+SWEP.WorldModel = Model("models/weapons/tf2_reshed/r_w_scorpion.mdl")
+
+SWEP.HoldType = "ar2"
+SWEP.LowerHoldType = "passive"
+
+SWEP.Firemode = -1
+
+SWEP.Primary = {
+	Ammo = "smg1",
+
+	ClipSize = 30,
+	DefaultClip = 20,
+
+	Damage = 21,
+	Delay = 60 / 1000,
+
+	Range = 3000,
+	Accuracy = 12,
+
+	RangeModifier = 0.98,
+
+	Recoil = {
+		MinAng = Angle(0, 0, 0),
+		MaxAng = Angle(0.5, 0.2, 0),
+		Punch = 0.4,
+		Ratio = 0.6
+	},
+
+	Sound = "weapons/doom_sniper_smg.wav",
+	TracerName = "Tracer"
+}
+
+
+
+-- Dynamic Weapon Reverb support
+SWEP.dwr_customAmmoType = "smg1"
+
+
+
+sound.Add( {
+	name = "rshd.Scorpion_magOut",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 80,
+	pitch = {95, 110},
+	sound = "weapons/smg_clip_out.wav"
+} )
+
+sound.Add( {
+	name = "rshd.Scorpion_magIn",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 80,
+	pitch = {95, 110},
+	sound = "weapons/smg_clip_in.wav"
+} )
+
+sound.Add( {
+	name = "rshd.Scorpion_boltBack",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 80,
+	pitch = {95, 110},
+	sound = "weapons/widow_maker_pump_action_back.wav"
+} )
+
+sound.Add( {
+	name = "rshd.Scorpion_boltFwd",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 80,
+	pitch = {95, 110},
+	sound = "weapons/widow_maker_pump_action_forward.wav"
+} )
